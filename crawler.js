4e8ea27ed82162af3d65d0c77b3535d551aa844d
joaopmartins <jopazmar@gmail.com>
2016-05-18 14:47:13 +0100

    #Jm# Added link for more stats

--- crawler.js
+++ crawler.js
@@ -3,15 +3,18 @@ var cheerio = require('cheerio');
 var URL = require('url-parse');
 var fs = require('fs');
 var path = require('path');
-
+http://www.meusresultados.com/jogo/xjJQrJ37/#sumario-do-jogo
   var $ = cheerio.load(fs.readFileSync('meusres.html'));
   $('#fs-results table tr').each(function( index ) {
     var round = $(this).find('td[colspan=6]').text().trim();
     var home = $(this).find('td > span.padr').text().trim();
     var away = $(this).find('td > span.padl').text().trim();
     var score = $(this).find('td.cell_sa').text().trim();
+    var matchId = $(this).attr('id');
+    if(matchId)
+    var link = 'http://www.meusresultados.com/jogo/'+matchId.substring(4)+'/';
     if(home && away && score)
-    fs.appendFileSync('meus.txt', "Home: " +home + ' |'+score+'| ' + away +'\n');
+    fs.appendFileSync('meus.txt', "Home: " +home + ' |'+score+'| ' + away +'\n'+'Link: '+link+'\n');
     if(round)
       fs.appendFileSync('meus.txt', "Round: " +round+'\n');
   });
